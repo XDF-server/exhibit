fcecb1f6d8f00788967fe9c363424071943c61fb
root <root@kickstart-151.(none)>
2015-08-09 10:23:41 +0800

    add

---
+++ static/js/new_old_question_show.js
@@ -0,0 +1,17 @@
+$(function(){
+
+	$('#body_error').on('click', function () {
+    		var $btn = $(this).button('loading')
+		//$btn.button('reset')
+	})
+		
+	$('#answer_error').on('click', function () {
+    		var $btn = $(this).button('loading')
+		//$btn.button('reset')
+	})
+	
+	$('#analysis_error').on('click', function () {
+    		var $btn = $(this).button('loading')
+		//$btn.button('reset')
+	})
+});
